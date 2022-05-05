c581bd9f314aa0a04ce4f80d105366e5c85bbb8c
flying-solo <flyingsoloharshit@gmail.com>
2022-05-06 01:58:03 +0530

    💄 add loader

--- src/pages/home/hero.js
+++ src/pages/home/hero.js
@@ -2,22 +2,28 @@ import HeroCenter from "../components/heroCenter";
 import HeroLeft from "../components/heroLeft";
 import HeroRight from "../components/heroRight";
 import useFetch from "../../hooks/useFetch";
+import { JellyTriangle } from '@uiball/loaders'
 // import { BallTriangle } from "react-loader-spinner";
 
 
 function Hero(props) {
-
   const { FetchedData, Fetching, Error } = useFetch(
     `https://alexa-news-api.herokuapp.com/harshit?category=${props.Domain}`);
 
   return (
     <div>
       {/* <BallTriangle color="#00BFFF" height={80} width={80} /> */}
+      {Fetching ? <div className="flex justify-center items-center mt-48">
+        <JellyTriangle size={60} speed={1.75} color="black"/>
+      </div> :
+      
+      (
       <div className="flex justify-center items-top mt-3 gap-2 mx-auto maxWidth">
         <HeroLeft FetchedData={FetchedData} Fetching={Fetching} Error={Error} />
         <HeroCenter FetchedData={FetchedData} Fetching={Fetching} Error={Error} />
         <HeroRight FetchedData={FetchedData} Fetching={Fetching} Error={Error} />
       </div>
+      )}
     </div>
   );
 }
